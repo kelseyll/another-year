6c8f73792e668d7efab5643eed2da801d650bbd9
Kelsey Legault <kelsey.legault@gmail.com>
2017-03-22 13:20:49 -0700

    Add js and hover function

---
+++ js/main.js
@@ -0,0 +1,7 @@
+console.log("dfdf");
+
+$(".overlay span").hover(function() {
+    console.log("bleeee");
+    $( this ).addClass("imbluedadadee");
+    $( this ).sibling().addClass("imbluedadadee");
+});
