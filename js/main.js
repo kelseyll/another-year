bb1cc5a3d24283e89dc61297255eb1bded832d2c
Kelsey Legault <kelsey.legault@gmail.com>
2017-04-09 16:32:54 -0700

    Disable infinite scroll on mobile

--- js/main.js
+++ js/main.js
@@ -4,12 +4,14 @@ $( document ).ready(function() {
     function scrollToElement(scrollto) {
         $(window).scrollTop(scrollto.offset().top).scrollLeft(scrollto.offset().left);
     }
-    $(document).scroll(function() {
-        if ($(window).scrollTop() + $(window).height() == $(document).height()) {
-            $(document).scrollTop(0);
-        }
-        else if($(window).scrollTop() == 0) {
-            $(document).scrollTop($(document).height());
-        }
-    });
+    if ( $(window).width() > 750) {
+        $(document).scroll(function() {
+            if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+                $(document).scrollTop(0);
+            }
+            else if($(window).scrollTop() == 0) {
+                $(document).scrollTop($(document).height());
+            }
+        });
+    }
 });
