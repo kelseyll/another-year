6be509c6bda132a4859c12a5b59d03c3d3a16252
Kelsey Legault <kelsey.legault@gmail.com>
2017-04-08 20:25:57 -0700

    Add fonts

--- js/main.js
+++ js/main.js
@@ -5,12 +5,5 @@ $( document ).ready(function() {
     $(document).scroll(function() {
         if(document.documentElement.clientHeight +
         $(document).scrollTop() >= document.body.offsetHeight )$(document).scrollTop(0);
-
-        if(document.documentElement.clientHeight +
-        $(document).scrollBottom() >= document.body.offsetHeight )$(document).scrollBottom(0);
     });
-    // $(document).scroll(function() {
-    //     if(document.documentElement.clientHeight +
-    //     $(document).scrollTop() >= document.body.offsetHeight )$(document).scrollTop(0);
-    // });
 });
