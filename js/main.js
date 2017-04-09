32ac113ab64b222f76b1e6dd8d9b9afd32e248a0
Kelsey Legault <kelsey.legault@gmail.com>
2017-04-08 20:20:42 -0700

    Add stairs scrolling

--- js/main.js
+++ js/main.js
@@ -2,4 +2,15 @@ $( document ).ready(function() {
     $(".overlay span").hover(function() {
         $( this ).addClass("imbluedadadee");
     });
+    $(document).scroll(function() {
+        if(document.documentElement.clientHeight +
+        $(document).scrollTop() >= document.body.offsetHeight )$(document).scrollTop(0);
+
+        if(document.documentElement.clientHeight +
+        $(document).scrollBottom() >= document.body.offsetHeight )$(document).scrollBottom(0);
+    });
+    // $(document).scroll(function() {
+    //     if(document.documentElement.clientHeight +
+    //     $(document).scrollTop() >= document.body.offsetHeight )$(document).scrollTop(0);
+    // });
 });
